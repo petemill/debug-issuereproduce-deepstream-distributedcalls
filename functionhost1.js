73a4b18661befa36fdea2e5db06a14fd9f0acc06
petemill <miller.pete+commit@gmail.com>
2016-01-08 16:55:28 -0800

    test functions with delay and large payload

--- functionhost1.js
+++ functionhost1.js
@@ -5,9 +5,12 @@ var deepstream = require( 'deepstream.io-client-js' ),
 ds.rpc.provide( 'testfn1', function( data, response ){
 
     console.log('Run testfn1 for ' + data.name);
-
+    setTimeout(function(){
+      console.log('testfn1 processing finished');
+      response.send( data.name + " processed" );
+    }, 3000);
     //you can now either send a response
-    response.send( data.name + " processed" );
+
 
     //reject the response so that it gets
     //re-routed to another provider
@@ -23,7 +26,7 @@ ds.rpc.provide( 'testfn3', function( data, response ){
     console.log('Run testfn3 for ' + data.name);
 
     //you can now either send a response
-    response.send( data.name + " processed" );
+    response.send( data.name + " processed " + bigString );
 
     //reject the response so that it gets
     //re-routed to another provider
@@ -32,3 +35,30 @@ ds.rpc.provide( 'testfn3', function( data, response ){
     //or send an error back
     //response.error( 'Something went wrong' );
 });
+//register as a provider for a rpc
+ds.rpc.provide( 'testfn5', function( data, response ){
+
+    console.log('Run testfn3 for ' + data.name);
+
+    //you can now either send a response
+    response.send( data.name + " processed " );
+
+    //reject the response so that it gets
+    //re-routed to another provider
+    //response.reject();
+
+    //or send an error back
+    //response.error( 'Something went wrong' );
+});
+
+
+function BigString(){
+  var x = "1234567890";
+  var iterations = 7;
+  for (var i = 0; i < iterations; i++) {
+    x += x+x;
+  }
+  return x;
+}
+
+var bigString = BigString();
--- functionhost2.js
+++ functionhost2.js
@@ -6,8 +6,10 @@ var deepstream = require( 'deepstream.io-client-js' ),
 
         console.log('Run testfn2 for ' + data.name);
 
-        //you can now either send a response
-        response.send( data.name + " processed" );
+        setTimeout(function(){
+          console.log('testfn2 processing finished');
+          response.send( data.name + " processed" );
+        }, 3000);
 
         //reject the response so that it gets
         //re-routed to another provider
@@ -19,6 +21,21 @@ var deepstream = require( 'deepstream.io-client-js' ),
 
     //register as a provider for a rpc
     ds.rpc.provide( 'testfn3', function( data, response ){
+
+        console.log('Run testfn3 for ' + data.name);
+
+        //you can now either send a response
+        response.send( data.name + " processed" );
+
+        //reject the response so that it gets
+        //re-routed to another provider
+        //response.reject();
+
+        //or send an error back
+        //response.error( 'Something went wrong' );
+    });
+    //register as a provider for a rpc
+    ds.rpc.provide( 'testfn4', function( data, response ){
 
         console.log('Run testfn3 for ' + data.name);
 
