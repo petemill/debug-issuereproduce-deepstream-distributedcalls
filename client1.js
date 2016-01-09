68093dfc55a1bc6837cd038f536c18da790c6e9b
petemill <miller.pete+commit@gmail.com>
2016-01-08 16:55:52 -0800

    client connection pooling, ops/second measurement

--- client1.js
+++ client1.js
@@ -1,50 +1,63 @@
 var deepstream = require( 'deepstream.io-client-js' ),
-    ds = deepstream( 'localhost:6021' ).login();
+singleConnection = deepstream('localhost:6021').login();
 
-    //other clients can now call this function using
-    ds.rpc.make( 'testfn1', { name: 'Any Name' }, function( error, response ){
-        //response is 11 now
-        if (error)
-          console.error(error);
-        console.log('Processed testfn1. Response ', response);
-    });
+var poolModule = require('generic-pool');
+var pool = poolModule.Pool({
+    name     : 'deepstream',
+    create   : function(callback) {
+        var c = deepstream( 'localhost:6021' ).login();
+        // parameter order: err, resource
+        // new in 1.0.6
+        callback(null, c);
+    },
+    destroy  : function(client) { client.close(); },
+    max      : 30,
+    // optional. if you set this, make sure to drain() (see step 3)
+    min      : 0,
+    // specifies how long a resource can stay idle in pool before being removed
+    idleTimeoutMillis : 30000,
+     // if true, logs via console.log - can also be a function
+    log : false
+});
 
-    //other clients can now call this function using
-    ds.rpc.make( 'testfn2', { name: 'Any Name' }, function( error, response ){
-        //response is 11 now
-        if (error)
-          console.error(error);
-        console.log('Processed testfn2. Response ', response);
-    });
+var lastRoundStart;
+var operationsThisRound;
+function MeasureOperationsInRound() {
+  lastRoundStart = process.hrtime();
+  operationsThisRound = 0;
+  setTimeout(function(){
+    var opsLastRound = operationsThisRound;
+    var elapsed = process.hrtime(lastRoundStart);
+    MeasureOperationsInRound();
+    console.log('BENCHMARK----------- ' + opsLastRound + ' ops in ' + (elapsed[0] * 1000 + elapsed[1]/1000000) + "ms");
+  }, 1000);
+}
+MeasureOperationsInRound();
 
+function MakeRepeatedCall(fnName) {
+  //setTimeout(function(){
     //other clients can now call this function using
-    ds.rpc.make( 'testfn3', { name: 'Any Name' }, function( error, response ){
-        //response is 11 now
-        if (error)
-          console.error(error);
-        console.log('Processed testfn3. Response ', response);
-    });
+    //var start = process.hrtime();
+//    pool.acquire(function(err, dsConnection){
 
-    //other clients can now call this function using
-    ds.rpc.make( 'testfn2', { name: 'Any Name' }, function( error, response ){
-        //response is 11 now
-        if (error)
-          console.error(error);
-        console.log('Processed testfn2. Response ', response);
-    });
+      //pool.release(dsConnection);
 
-    //other clients can now call this function using
-    ds.rpc.make( 'testfn1', { name: 'Any Name' }, function( error, response ){
-        //response is 11 now
-        if (error)
-          console.error(error);
-        console.log('Processed testfn1. Response ', response);
-    });
-
-    //other clients can now call this function using
-    ds.rpc.make( 'testfn3', { name: 'Any Name' }, function( error, response ){
-        //response is 11 now
-        if (error)
-          console.error(error);
-        console.log('Processed testfn3. Response ', response);
-    });
+      singleConnection.rpc.make( fnName, { name: 'Any Name' }, function( error, response ){
+        operationsThisRound++;
+        //var elapsed = process.hrtime(start);
+        //console.log(fnName + " " +(elapsed[0] * 1000 + elapsed[1]/1000000) + "ms");
+          //response is 11 now
+          if (error)
+            console.error(error);
+        //  console.log('Processed ' + fnName + '. Response ', response);
+          //repeat
+          MakeRepeatedCall(fnName);
+      });
+//    });
+  //}, 1);
+}
+var fnAll = 'testfn5';
+var parallelCalls = 30;
+for (var i = 0; i < parallelCalls; i++) {
+  MakeRepeatedCall(fnAll);
+}
